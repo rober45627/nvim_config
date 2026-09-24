0ad7d3ef39a47e47f051e3b4cf42e09d5aba99cb
rober45627 <robrory@outlook.ie>
2026-09-24 20:47:36 +0100

    added synctex functionality

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,21 +1,39 @@
 return {
   "lervag/vimtex",
-  lazy = false,
   init = function()
     vim.g.vimtex_compiler_method = "latexmk"
-    vim.g.vimtex_syntax_conceal_disable = true
+
     vim.g.vimtex_quickfix_ignore_filters = {
       "Overfull \\\\hbox",
       "Underfull \\\\hbox",
       "Overfull \\\\vbox",
       "Underfull \\\\vbox",
     }
+
     vim.g.vimtex_compiler_latexmk = {
       options = {
-        "-pdf",
+        "-verbose",
+        "-file-line-error",
         "-interaction=nonstopmode",
-        "-synctex=0",
+        "-synctex=1",
       },
+      clean_ext = "synctex.gz synctex",
     }
+    vim.g.vimtex_compiler_progname = "nvr"
+
+    vim.g.vimtex_syntax_conceal_disable = true
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "tex",
+      callback = function(args)
+        vim.keymap.set("n", "<localleader>ld", function()
+          vim.cmd("VimtexStop")
+          vim.cmd("VimtexClean")
+        end, {
+          buffer = args.buf,
+          desc = "VimTeX Stop and Clean",
+        })
+      end,
+    })
   end,
 }
